349ba7645e1309c56d6a93663f69179f227a0f27
ooyama <ooyama@m-g-n.me>
2024-03-12 01:23:03 +0900

    プロジェクトのデフォルト担当者を設定する

--- prod/set_default_assignee.js
+++ prod/set_default_assignee.js
@@ -8,4 +8,26 @@ function set_default_assignee() {
     const API_KEY = scriptProperties.getProperty('API_KEY');
     const ID_OOYAMA = scriptProperties.getProperty('ID_OOYAMA');
 
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    const sheet_todo = ss.getSheetByName('00_not_yet_assign');
+
+    // sheet_todoのD2:DからissueKeyを取得
+    const issueKey_list = sheet_todo.getRange(2, 4, sheet_todo.getLastRow()-1, 1).getValues().flat();
+    Logger.log(issueKey_list);
+
+    // issueKeyごとに担当者を設定
+    for (let i = 0; i < issueKey_list.length; i++) {
+        let url = `https://${SPACE_KEY}.backlog.jp/api/v2/issues/${issueKey_list[i]}?apiKey=${API_KEY}`;
+        const payload = {
+            assigneeId: ID_OOYAMA
+        };
+        const options = {
+            method: 'patch',
+            contentType: 'application/json',
+            payload: JSON.stringify(payload)
+        };
+        const res = UrlFetchApp.fetch(url, options);
+        Logger.log(res);
+    }
+
 }
